4a461b6c76bf9431ae682cefbe095c922c01caee
julissa93 <julissa.napoletano@cix.csi.cuny.edu>
2018-04-18 15:56:45 -0400

    started php functions

--- fcns.php
+++ fcns.php
@@ -1,2 +1,27 @@
 <?php
+function connect()
+{
+  $servername = "localhost";
+  $username = "root";
+  $password = "root";
+  $db = "tutoring_mgmt_sys";
+  $result = new mysqli($servername, $username, $password, $db);
+   if (mysqli_connect_errno())
+   {
+     throw new Exception('Could not connect to database server');
+   }
+   else
+   {
+     echo "Connected successfully";
+     return $result;
+   }
+}
+
+function login($username, $password, $acctype)
+{
+  $session_start();
+  $_SESSION['login_user'] = $username;
+  $_SESSION['password'] = $password;
+
+}
 ?>
