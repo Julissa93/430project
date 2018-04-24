9b1915f881aa0959bec6a60a51de4d5b0abaa5e0
julissa93 <julissa.napoletano@cix.csi.cuny.edu>
2018-04-24 17:56:46 -0400

    changed filename

--- login.php
+++ login.php
@@ -31,7 +31,6 @@ else
       // if they are in the database register the user id
           session_start();
           $_SESSION['valid_user'] = $username;
-          echo "Success betch!!!!!!!!!!!!sjlsdfgjlgsjblgf";
           header("Location:homepage.php");
       }
       else
