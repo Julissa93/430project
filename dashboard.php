47e9d96564762ed4301946bf67878edc59a88559
KailieYuan <31041546+KailieYuan@users.noreply.github.com>
2018-04-25 23:02:38 -0400

    colors were added to the page
    we might need to throw it in the css file?
    button was moved to the navigation panel

--- dashboard.php
+++ dashboard.php
@@ -1,74 +1,76 @@
 <?php
-session_start();
-include('fcns.php');
-showHeader();
-?>
-
-  <div class = "row d-flex align-items-center" style = "background-color: #111; padding-right: 0 !important; margin-right: 0 !important;   ">
-    <div class = "col-2">
-      <div class="sidenav" >
-        <div class = "text-center text-primary"><i class="fas fa-graduation-cap fa-7x"></i></div>
-        &nbsp;
-        <a href="searchPerson.php">Users</a>
-        <a href="#services">Billing</a>
-        <a href="#clients">Submit</a>
-        <button class="dropdown-btn">Dropdown
-          <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-container">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
-        </div>
-        <button class="dropdown-btn">Dropdown
-          <i class="fa fa-caret-down"></i>
-        </button>
-        <div class="dropdown-container">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
-        </div>
+   session_start();
+   include('fcns.php');
+   showHeader();
+   ?>
+<div class = "row d-flex align-items-center" style = "background-color: #64F7B0"; padding-right: 0 !important; margin-right: 0 !important;   ">
+<div class = "col-2">
+   <div class="sidenav"style = "background-color: #05B5BA">
+      <div class = "text-center text-primary"><i class="fas fa-graduation-cap fa-7x" style = "background-color: #64F7B0"></i></div>
+      &nbsp;
+      <?php echo "<h2 style = 'color: white; '> Hello, ".$_SESSION['valid_user']."</h2>"; ?>
+      <button class="dropdown-btn" >Users
+      <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-container" style = "background-color: #64F7B0">
+         <a href="addPerson.php">Add Person</a>
+         <a href="searchPerson.php">Search Person</a>
+         <a href="#">Remove Person</a>
       </div>
-    </div>
-    <div class = "col-8">
-      <?php echo "<h1 class = 'text-center' style = 'color: white; '> Hello, ".$_SESSION['valid_user']."</h1>"; ?>
-    </div>
-    <div class = "col-2">
-      <button type = "button" class = "btn btn-primary float-right"  ><i class="fas fa-sign-out-alt"></i> Sign Out</button>
-    </div>
-
-
-
-
-  </div>
-
-
+      <button class="dropdown-btn">Billing
+      <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-container" style = "background-color: #64F7B0">
+         <a href="#">Register</a>
+         <a href="#">Edit</a>
+         <a href="#">Refund</a>
+      </div>
+      <button class="dropdown-btn">Submit
+      <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-container" style = "background-color: #64F7B0">
+         <a href="#">Attendance</a>
+         <a href="#">Grades</a>
+      </div>
+      <button class="dropdown-btn">Our Mission
+      <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-container" style = "background-color: #64F7B0">
+         <a href="aboutUs.html">About Us</a>
+         <a href="hoursOfOperation.html">Hours of Operation</a>
+         <a href="classInfo.html">This Semester</a>
+      </div>
+      <button type = "button" class = "btn btn-primary float-left"  ><i class="fas fa-sign-out-alt"></i> Sign Out</button>
+   </div>
+</div>
+<div class = "col-2">
+</div>
+</div>
 <script>
-//* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-var dropdown = document.getElementsByClassName("dropdown-btn");
-var i;
-
-for (i = 0; i < dropdown.length; i++) {
-  dropdown[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var dropdownContent = this.nextElementSibling;
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
-    }
-  });
-}
-
-$('a').click(function(ev){
-    ev.preventDefault();
-
-    // hide everything
-   $('.box').not(
-       // except the corresponding box
-       $('.box.'+$(this).data('box')+':hidden').fadeIn()
-   ).fadeOut();
-});
+   //* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+   var dropdown = document.getElementsByClassName("dropdown-btn");
+   var i;
+   
+   for (i = 0; i < dropdown.length; i++) {
+     dropdown[i].addEventListener("click", function() {
+       this.classList.toggle("active");
+       var dropdownContent = this.nextElementSibling;
+       if (dropdownContent.style.display === "block") {
+         dropdownContent.style.display = "none";
+       } else {
+         dropdownContent.style.display = "block";
+       }
+     });
+   }
+   
+   $('a').click(function(ev){
+       ev.preventDefault();
+   
+       // hide everything
+      $('.box').not(
+          // except the corresponding box
+          $('.box.'+$(this).data('box')+':hidden').fadeIn()
+      ).fadeOut();
+   });
 </script>
-
 <?php showFooter();  ?>
