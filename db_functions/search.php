878dc0925f4b88fe1312680e99b46dc0d9bb81b6
julissa93 <julissa.napoletano@cix.csi.cuny.edu>
2018-04-27 19:53:43 -0400

    changing ish

---
+++ db_functions/search.php
@@ -0,0 +1,47 @@
+<?php
+//session_start();
+include('fcns.php');
+
+
+    $conn = connect();
+    $userid = trim($_POST['search']);
+    $userid = htmlspecialchars($userid);
+
+    $sql = "select name, location, type, username from user where user_id = $userid";
+    $stmt = $conn->query($sql);
+    /*$stmt = $conn->prepare($sql);
+    $stmt->bind_param('s', $userid);
+    $stmt->execute();*/
+
+
+    if($stmt->num_rows > 0)
+    {
+      while($row = $stmt->fetch_assoc())
+      {
+        //showHeader();
+        echo"
+        &nbsp;
+          <div class = 'row d-flex justify-content-center'>
+            <div class = 'col-12'>
+            <p class = 'text-center align-middle'>
+
+              <strong>Name: </strong>".$row['name']."
+
+              <strong>User Type: </strong> ".$row['type']."</p>
+            </div>
+          </div>
+        ";
+        //showFooter();
+      }
+    }
+
+
+  /*else
+  {
+      echo"<div class='alert alert-danger' role='alert'>
+      <strong>Oh snap!</strong> Change a few things up and try submitting again.
+      </div>";
+  }*/
+
+
+?>
---
+++ forms/addPerson.php
@@ -0,0 +1,63 @@
+<?php
+include('fcns.php');
+showHeader();  
+?>
+      <div class="container" style="max-width:600px;padding:40px 20px;background:#ebeff2; ">
+         <h3 class = "text-center">Add Person</h3>
+         <form method = "post" class="form-horizontal" role="form" action = "add.php">
+            <div class="form-group">
+               <label for="firstName" class ="control-label col-sm-3">First name</label>
+               <div class="col-sm-8">
+                  <input name = "first_name" type="name" class="form-control" id="firstName" placeholder="First Name">
+               </div>
+            </div>
+            <div class="form-group">
+               <label for="lastName" class ="control-label col-sm-3">Last Name</label>
+               <div class="col-sm-8">
+                  <input name ="last_name" type="text" class="form-control" id="lastName" placeholder="Last Name">
+               </div>
+            </div>
+            <div class="form-group">
+               <label for="location" class ="control-label col-sm-3">Location</label>
+               <div class="col-sm-8">
+                  <select id = "location" class="form-control" name="location">
+                     <option value="location1">Location 1</option>
+                     <option value="location2">Location 2</option>
+                     <option value="location3">Location 3</option>
+                  </select>
+               </div>
+            </div>
+            <div class="form-group">
+               <label for="type" class ="control-label col-sm-3">Type of Person</label>
+               <select id = "type" class="form-control" name="user_type" onchange = 'determineType(this.value);'>
+                  <option style = "white-space: nowrap; ">Type of Person</option>
+                  <option value="manager">Manager</option>
+                  <option value="employee">Employee</option>
+                  <option value = "student"> Student </option>
+               </select>
+            </div>
+            <div class="form-group">
+               <input type="email" style="display:none" class="form-control" id="email" placeholder="Enter email">
+            </div>
+            <br/>
+            <div class="col-sm-offset-2 col-sm-8">
+               <button type='submit' class='btn btn-primary' name = 'submit'>Submit</button>
+            </div>
+         </form>
+      </div>
+      </div>
+      <!-- Optional JavaScript -->
+      <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<?php showFooter(); ?>
+   <script>
+      function determineType(val) {
+          var type_selected = document.getElementById('type');
+          var email = document.getElementById("email");
+          if (val == 'student'){
+              email.style.display = "block";
+          } else {
+             email.style.display = "none";
+          }
+      }
+   </script>
+</html>
