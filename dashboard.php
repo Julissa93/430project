524ca7a41b2674f327cf015109dd8d829b0272ab
julissa93 <julissa.napoletano@cix.csi.cuny.edu>
2018-04-27 16:54:03 -0400

    fixed css and added search bills sql file

--- dashboard.php
+++ dashboard.php
@@ -4,45 +4,45 @@
    showHeader();
    ?>
 <div class = "row d-flex align-items-center" style = "background-color: #64F7B0; padding-right: 0 !important; margin-right: 0 !important;   ">
-<div class = "col-2">
-   <div class="sidenav"style = "background-color: #3a4651">
-      <div class = "text-center text-primary"><i class="fas fa-graduation-cap fa-7x" ></i></div>
-      &nbsp;
-      <?php echo "<h2 style = 'color: white; '> Hello, ".$_SESSION['valid_user']."</h2>"; ?>
-      <button class="dropdown-btn" >Users
-      <i class="fa fa-caret-down"></i>
-      </button>
-      <div class="dropdown-container">
-         <a href="#addPerson" id = "addPerson">Add Person</a>
-         <a href="#searchUsers" id = "searchUsers">Search Person</a>
-         <a href="#">Remove Person</a>
-      </div>
-      <button class="dropdown-btn">Billing
-      <i class="fa fa-caret-down"></i>
-      </button>
-      <div class="dropdown-container" style = "background-color: #64F7B0">
-         <a href="#">Register</a>
-         <a href="#">Edit</a>
-         <a href="#">Refund</a>
-      </div>
-      <button class="dropdown-btn">Submit
-      <i class="fa fa-caret-down"></i>
-      </button>
-      <div class="dropdown-container" style = "background-color: #64F7B0">
-         <a href="#">Attendance</a>
-         <a href="#">Grades</a>
-      </div>
-      <button class="dropdown-btn">Our Mission
-      <i class="fa fa-caret-down"></i>
-      </button>
-      <div class="dropdown-container" style = "background-color: #64F7B0">
-         <a href="aboutUs.html">About Us</a>
-         <a href="hoursOfOperation.html">Hours of Operation</a>
-         <a href="classInfo.html">This Semester</a>
-      </div>
-      <button type = "button" class = "btn btn-primary float-left"  ><i class="fas fa-sign-out-alt"></i> Sign Out</button>
-   </div>
-</div>
+  <div class = "col-2">
+     <div class="sidenav"style = "background-color: #3a4651">
+        <div class = "text-center text-primary"><i class="fas fa-graduation-cap fa-7x" ></i></div>
+        &nbsp;
+        <?php echo "<h2 style = 'color: white; '> Hello, ".$_SESSION['valid_user']."</h2>"; ?>
+        <button class="dropdown-btn" >Users
+        <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-container">
+           <a href="#addPerson" id = "addPerson">Add</a>
+           <a href="#searchUsers" id = "searchUsers">Search</a>
+           <a href="#">Edit</a>
+        </div>
+        <button class="dropdown-btn">Billing
+        <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-container" >
+           <a href="#">Search</a>
+           <a href="#">Edit</a>
+           <a href="#">Refund</a>
+        </div>
+        <button class="dropdown-btn">Submit
+        <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-container" >
+           <a href="#">Attendance</a>
+           <a href="#">Grades</a>
+        </div>
+        <button class="dropdown-btn">Our Mission
+        <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-container" >
+           <a href="aboutUs.html">About Us</a>
+           <a href="hoursOfOperation.html">Hours of Operation</a>
+           <a href="classInfo.html">This Semester</a>
+        </div>
+        <button type = "button" class = "btn btn-primary float-left"  ><i class="fas fa-sign-out-alt"></i> Sign Out</button>
+     </div>
+  </div>
 <div class = "col-10">
   <div id = "display">
   </div>
