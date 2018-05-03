152504d11963a8577ab2c485378d56b31c6c610a
sistemalberto19 <sistemalberto@gmail.com>
2018-05-03 15:36:22 -0400

    adding margin-top and form title

--- forms/addPerson.php
+++ forms/addPerson.php
@@ -11,20 +11,26 @@ include('../db_functions/fcns.php');
       
       <head>
       <link href="http://cdn.materialdesignicons.com/2.1.99/css/materialdesignicons.min.css" rel="stylesheet" />
-      <div class="container">
-         <div class="row">
+
+         <div class="row align-items-center justify-content-center" style="margin-top:60px">
+       
             <div class="col-lg-4 col-md-4 mx-auto">
                <div class="card">
-                  <div class="card-body">
-                     <form method = "post" class="form-horizontal" role="form" action = "" id = "addPersonForm">
+                  <div class="card-body">                  
+                  <h3>Register Employee</h3>
+                   
+                    <form method = "post" class="form-horizontal" role="form" action = "" id = "addPersonForm">
+                       
                         <div class="form-group">
                            <label for="firstName" class ="control-label">First Name<span class="text-danger">*</span></label>
                            <input name = "first_name" type="text" class="form-control" id="firstName" placeholder="Enter First Name">
                         </div>
+                        
                         <div class="form-group">
                            <label for="lastName" class ="control-label">Last Name <span class="text-danger">*</span></label>
                            <input name ="last_name" type="text" class="form-control" id="lastName" placeholder="Enter Last Name">
                         </div>
+                        
                         <div class="form-group">
                            <label for="location" class ="control-label">Location <span class="text-danger">*</span></label>
                            <select id = "location" class="form-control" name="location">
@@ -33,6 +39,7 @@ include('../db_functions/fcns.php');
                               <option value="location3">Location 3</option>
                            </select>
                         </div>
+                        
                         <div class="form-group">
                            <label for="type" class ="control-label">Type of Person<span class="text-danger">*</span></label>
                            <select id = "type" class="form-control" name="user_type" onchange = 'determineType(this.value);'>
@@ -41,20 +48,22 @@ include('../db_functions/fcns.php');
                               <option value="employee">Employee</option>
                            </select>
                         </div>
+                        
                         <div class="form-group">
                            <label for="pin" class ="control-label">Password<span class="text-danger">*</span></label>
                            <input name ="pin" type="password" class="form-control" id="pin" placeholder="Enter Password">
                         </div>
-                        <div class="form-group">
-                        </div>
-                        <button type="submit" class="btn btn-success btn-block" name = 'submit' id = "submit">Submit</button>
+                                              
+                        <button type="submit" class="btn btn-primary btn-block" name = 'submit' id = "submit">Submit</button>
+                        
                      </form>
                   </div>
                </div>
             </div>
          </div>
       </div>
-<?php showFooter(); ?>
+            
+    <?php showFooter(); ?>
    <script>
       function determineType(val) {
           var type_selected = document.getElementById('type');
