8fee5467d07dd9db33de9bbadc2f4a2f8686a6bb
KailieYuan <31041546+KailieYuan@users.noreply.github.com>
2018-05-04 21:20:01 -0400

    Template is done
    Need to get reveal_options working.  Only grades 3-8 should have subject and level, shsat and psat and sat should have day.  All should have grade and location.
    
    The table should be based on queries in a loop to display all students.

--- forms/submitAttendance.php
+++ forms/submitAttendance.php
@@ -9,12 +9,52 @@
       <title>Submit Attendance</title>
    </head>
    <body data-spy="scroll" data-target=".navbar" data-offset="50">
+      <h3><label for="grade" class ="control-label">Grade:</label></h3>
+      <select id = "grade" class="form-control" name="grade" onclick="reveal_options(value)">
+         <option style = "white-space: nowrap; ">Grade</option>
+         <option value="3">3</option>
+         <option value="4">4</option>
+         <option value="5">5</option>
+         <option value="6">6</option>
+         <option value="7">7</option>
+         <option value="8">8</option>
+         <option value="shsat">SHSAT</option>
+         <option value="psat">PSAT</option>
+         <option value="sat">SAT</option>
+      </select>
+      <h3><label for="subject" class ="control-label">Subject:</label></h3>
+      <select id = "subject" class="form-control" name="subject">
+         <option style = "white-space: nowrap; ">Subject</option>
+         <option value="english">English</option>
+         <option value="math">Math</option>
+         <option value="writing">Writing</option>
+      </select>
+      <h3><label for="level" class ="control-label">Level:</label></h3>
+      <select id = "level" class="form-control" name="level">
+         <option style = "white-space: nowrap; ">Level</option>
+         <option value="1">1</option>
+         <option value="2">2</option>
+         <option value="3">3</option>
+      </select>
+      <h3><label for="day" class ="control-label">Day:</label></h3>
+      <select id = "day" class="form-control" name="day">
+         <option style = "white-space: nowrap; ">Day</option>
+         <option value="1">Saturday</option>
+         <option value="2">Sunday</option>
+      </select>
+      <h3><label for="location" class ="control-label">Location:</label></h3>
+      <select id = "location" class="form-control" name="location">
+         <option style = "white-space: nowrap; ">Location</option>
+         <option value="location1">Location 1</option>
+         <option value="location2">Location 2</option>
+         <option value="location3">Location 3</option>
+      </select>
       <h3>Submitting Attendance for ___</h3>
       <form class="form-horizontal" role="form">
          <div class="form-group">
             <!--We need to query all students in the same class -->
-			<!--We also need to see how we can put check boxes without titles in a single column after the name-->
-        </div>
+            <!--We also need to see how we can put check boxes without titles in a single column after the name-->
+         </div>
          <br/>
          <div class="col-sm-offset-2 col-sm-8">
             <button type="submit" class="btn btn-default">Submit</button>
@@ -22,7 +62,45 @@
       </form>
       </div>
       </div>
-      <!-- Optional JavaScript -->
+      <div class = "row justify-content-center">
+      <div class = "col-10">
+      &nbsp;
+      <table class = 'table'>
+         <thead>
+            <tr>
+               <th scope='col'>ID</th>
+               <th scope='col'>First</th>
+               <th scope='col'>Last</th>
+               <th scope='col'>Location</th>
+               <th scope='col'>Absent</th>
+            </tr>
+         </thead>
+         <tbody id = "showUsers">
+            <tr>
+               <th scope='col'>123456789</th>
+               <th scope='col'>John</th>
+               <th scope='col'>Doe</th>
+               <th scope='col'>1</th>
+               <th><input type="checkbox" id="is_absent" name="class[]" value="is_absent"></input></th>
+            </tr>
+            <tr>
+               <th scope='col'>569742368</th>
+               <th scope='col'>Peter</th>
+               <th scope='col'>Luger</th>
+               <th scope='col'>1</th>
+               <th><input type="checkbox" id="is_absent" name="class[]" value="is_absent"></input></th>
+            </tr>
+         </tbody>
+      </table>
+      <script>
+         var x = 0;
+                  function reveal_options(x) {
+         	if(x == shsat || x == psat || x == sat)
+                          document.getElementById("subject").style.visibility="none";
+                          document.getElementById("level").style.visibility="none";
+                  }
+      </script>
+	  <!-- Optional JavaScript -->
       <!-- jQuery first, then Popper.js, then Bootstrap JS -->
       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
