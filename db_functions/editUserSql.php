9b9753b0fc6236427ae77d7350d387bf8ad022a0
julissa93 <julissa.napoletano@cix.csi.cuny.edu>
2018-05-07 02:20:36 -0400

    fixed a boo boo

--- db_functions/editUserSql.php
+++ db_functions/editUserSql.php
@@ -5,11 +5,9 @@ $conn = connect();
 $userid = $_POST['userid'];
 $firstname = $_POST['firstname'];
 $lastname = $_POST['lastname'];
+$location = $_POST['location'];
 $type = $_POST['type'];
 
-echo "HELLO";
-echo $userid;
-echo "<br>";
 // Code to update all personal information and then set session varibales to newly entered inputs.
 $sql = "UPDATE user SET firstname = ?, lastname = ?, location = ?, type = ? WHERE user_id = ?";
 
