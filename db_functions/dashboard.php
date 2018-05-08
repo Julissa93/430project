62ddd966bbd8033cf86410cf178a8ab5b7658b7e
julissa93 <julissa.napoletano@cix.csi.cuny.edu>
2018-05-08 09:41:05 -0400

    added sign out

--- db_functions/dashboard.php
+++ db_functions/dashboard.php
@@ -52,7 +52,7 @@
         </div>
         <hr />
         <div class="container">
-          <a href = '../430project/index.php' class = "btn btn-primary float-left"  ><i class="fas fa-sign-out-alt"></i> Sign Out</a><br><br>
+          <a href = '../index.php' class = "btn btn-primary float-left"  ><i class="fas fa-sign-out-alt"></i> Sign Out</a><br><br>
         </div>
         <!--<div class="container">
           <button type = "button" class = "btn btn-primary float-left">Change Password</button>
